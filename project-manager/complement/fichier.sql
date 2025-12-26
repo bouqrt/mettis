19ff411d08655fb8b9280af468b3d428ac1c045c
bouqrt <bouchraelqrt@gmail.com>
2025-12-26 17:26:23 +0100

    addition of member table

--- project-manager/complement/fichier.sql
+++ project-manager/complement/fichier.sql
@@ -10,21 +10,4 @@ create table member (
     -> email varchar(100) unique not null
     -> );
 
-create table project (
-    -> id int auto_increment primary key,
-    -> title varchar(100) not null,
-    -> description text,
-    -> debutdate datetime,
-    -> idmember int not null,
-    -> foreign key (idmember) references member(id) on delete cascade
-    -> );
 
-create table activity (
-    -> id int auto_increment primary key,
-    -> description text,
-    -> activitydate datetime,
-    -> idproject int not null,
-    -> foreign key (idproject) references project(id) on delete cascade
-    -> );
-
-show tables;
